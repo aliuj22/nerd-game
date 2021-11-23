7b8e83a2904be87e4d7a9c590ad035a0f2c4b77c
Anna Sakurai <zenasak@gmail.com>
2021-11-23 22:26:40 +0100

    add shooting function

---
+++ src/Bullet.js
@@ -0,0 +1,36 @@
+import Phaser from 'phaser';
+
+export default class Bullet extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene) {
+    super(scene, 0, 0, 'bullet');
+  }
+
+  shoot(x, y) {
+    //this.scene.sound.play('shoot');
+    this.setCollideWorldBounds(true);
+    //this.body.onWorldBounds = true;
+    this.onWorldBounds = true;
+    this.enableBody(true, x, y, true, true);
+    this.setVelocityY(-300);
+  }
+
+  deactivate() {
+    this.disableBody(true, true);
+  }
+}
+// export default class Bullet extends Phaser.GameObjects.Image {
+//   speed: number;
+//   born: number;
+//   direction: number;
+//   xSpeed: number;
+//   ySpeed: number;
+//   constructor(scene: any, x: number, y: number) {
+//   super(scene, x, y, 'bullet');
+//   this.speed = 1;
+//   this.born = 0;
+//   this.direction = 0;
+//   this.xSpeed = 0;
+//   this.ySpeed = 0;
+//   this.setSize(12, 12);
+//   }
+// }
--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,9 +1,12 @@
 import Phaser from 'phaser';
 
-import TitleScreen from './TitleScreen';
+import Bullet from '../Bullet';
+//import TitleScreen from './TitleScreen';
 
 const Keys = ['Julia', 'Alex', 'Redbull'];
-let player, playerControls;
+let player, playerControls, fireButton, game;
+let bulletTime = 0;
+
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
     super('hello-world');
@@ -27,34 +30,55 @@ export default class HelloWorldScene extends Phaser.Scene {
       frameWidth: 64,
       frameHeight: 64,
     });
+    this.load.image('bullet', './assets/bullet.png');
   }
 
   create() {
     console.log(Keys[this.characterIndex]);
     this.add.image(400, 300, 'bg');
 
-    player = this.add.image(100, 100, Keys[this.characterIndex]);
+    player = this.physics.add.sprite(100, 100, Keys[this.characterIndex]);
+    player.setCollideWorldBounds(true);
     player.x = 400;
     player.y = 500;
     console.log('init is ', this.data);
     this.load.image('bg', './assets/big-bg.png');
-    this.load.spritesheet('invader1', './assets/c.png', {
-      frameWidth: 64,
-      frameHeight: 64,
-    });
-    var invader1 = this.add.group({
-      key: 'invader1',
-      frame: 0,
-      repeat: 8,
-      setXY: { x: 80, y: 100, stepX: 60 },
-    });
+
+    // this.load.spritesheet('invader1', './assets/c.png', {
+    //   frameWidth: 64,
+    //   frameHeight: 64,
+    // });
+    // var invader1 = this.add.group({
+    //   key: 'invader1',
+    //   frame: 0,
+    //   repeat: 8,
+    //   setXY: { x: 80, y: 3100, stepX: 60 },
+    // });
 
     this.physics.add.existing(player);
     playerControls = this.input.keyboard.createCursorKeys();
+
+    this.bullets = this.physics.add.group({
+      //the maximum number of bullets. 50 is fairly small and there will be pauses while firing waiting for fired bullets to recycle back into the available pool.
+      maxSize: -1,
+      classType: Bullet,
+      //Since the bullet needs to update its position runChildUpdate must be true.
+      runChildUpdate: true,
+    });
+
+    this.physics.world.on('worldbounds', this.onWorldbounds, this);
+
+    fireButton = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.SPACE
+    );
   }
 
   update() {
     this.movePlayer();
+
+    if (fireButton.isDown) {
+      this.fireBullet();
+    }
   }
 
   movePlayer() {
@@ -64,4 +88,31 @@ export default class HelloWorldScene extends Phaser.Scene {
       player.x += 10;
     }
   }
+
+  fireBullet() {
+    const bullet = this.bullets.get();
+    if (bullet) {
+      bullet.shoot(player.x, player.y - 150);
+    }
+  }
+  onWorldbounds(body) {
+    const isBullet = this.bullets.contains(body.gameObject);
+    if (isBullet) {
+      body.gameObject.deactivate();
+    }
+  }
+  //   fireBullet() {
+  //     //  To avoid them being allowed to fire too fast we set a time limit
+  //     if (this.time.now > bulletTime) {
+  //       //  Grab the first bullet we can from the pool
+  //       bullets = bullets.getFirstExists(false);
+
+  //       if (bullets) {
+  //         //  And fire it
+  //         bullets.reset(player.x, player.y + 8);
+  //         bullets.body.velocity.y = -400;
+  //         bulletTime = this.time.now + 200;
+  //       }
+  //     }
+  //   }
 }
