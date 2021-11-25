2b7303e968ea7fdc046fffd9ed8ccc7633446ffe
Anna Sakurai <zenasak@gmail.com>
2021-11-25 10:26:00 +0100

    character shows

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-const game = {};
-import Phaser from "phaser";
+//
+import Phaser from 'phaser';
 
-import TitleScreen from "./scenes/TitleScreen";
+import TitleScreen from './scenes/TitleScreen';
 
-import HelloWorldScene from "./scenes/HelloWorldScene";
+import HelloWorldScene from './scenes/HelloWorldScene';
 
-import GameOverScene from "./scenes/game-over.js";
+import GameOverScene from './scenes/game-over.js';
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     //   arcade: {
     //     gravity: false,
     //     // debug: true,
@@ -24,8 +24,8 @@ const config = {
   },
 };
 
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
 //game.scene.add('titlescreen', TitleScreen);
-game.scene.start("title-screen");
+game.scene.start('title-screen');
 //game.scene.add('over', GameOverScene);
--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,16 +1,14 @@
-let enemy, enemy2, enemy3, spaceSound, bg;
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
-import Bullet from "../Bullet";
+import Bullet from '../Bullet';
 //import TitleScreen from './TitleScreen';
 
-const Keys = ["Julia", "Alex", "Redbull"];
+const Keys = ['Julia', 'Alex', 'Redbull'];
 let player, playerControls, fireButton, game;
-let bulletTime = 0;
-
+let enemy, enemy2, enemy3, spaceSound, bg;
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
-    super("hello-world");
+    super('hello-world');
   }
 
   init(data) {
@@ -22,39 +20,38 @@ export default class HelloWorldScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.audio("space", "./assets/space.mp3");
-    this.load.spritesheet("invader1", "./assets/python.png", {
+    this.load.audio('space', './assets/space.mp3');
+    this.load.spritesheet('invader1', './assets/python.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.spritesheet("invader2", "./assets/python.png", {
+    this.load.spritesheet('invader2', './assets/python.png', {
       frameWidth: 44,
       frameHeight: 32,
     });
-    this.load.spritesheet("invader3", "./assets/python.png", {
+    this.load.spritesheet('invader3', './assets/python.png', {
       frameWidth: 48,
       frameHeight: 32,
     });
 
-    this.load.image("bg", "./assets/big-bg.png");
+    this.load.image('bg', './assets/big-bg.png');
     this.load.image(
       `${Keys[this.characterIndex]}`,
       `./assets/${Keys[this.characterIndex]}128.png`
     );
-    //  this.load.spritesheet('invader1', './assets/c.png', {
-    //    frameWidth: 64,
-    //    frameHeight: 64,
-    //  });
-    this.load.image("bullet", "./assets/bullet.png");
+
+    this.load.image('bullet', './assets/laser-red-2.png');
   }
 
   create() {
+    // background image is being created here
+    this.add.image(400, 300, 'bg');
+
     player = this.physics.add.sprite(100, 100, Keys[this.characterIndex]);
     player.setCollideWorldBounds(true);
     player.x = 400;
     player.y = 500;
-    console.log("init is ", this.data);
-    // this.load.image('bg', './assets/big-bg.png');
+    console.log('init is ', this.data);
 
     this.physics.add.existing(player);
     playerControls = this.input.keyboard.createCursorKeys();
@@ -67,23 +64,20 @@ export default class HelloWorldScene extends Phaser.Scene {
       runChildUpdate: true,
     });
 
-    this.physics.world.on("worldbounds", this.onWorldbounds, this);
+    this.physics.world.on('worldbounds', this.onWorldbounds, this);
 
     fireButton = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     );
 
-    // background image is being created here
-    this.add.image(400, 300, "bg");
     //  spaceSound = this.sound.add('space', { volume: 0.2 });
 
     // enemy 1 is being created below
-
     this.physics.world.setBoundsCollision();
     enemy = this.physics.add.group();
 
     for (var i = 0; i < 13; i++) {
-      var pineapple = enemy.create(200 + i * 48, 50, "invader1");
+      var pineapple = enemy.create(200 + i * 48, 50, 'invader1');
       //This allows your sprite to collide with the world bounds like they were rigid objects
       pineapple.body.collideWorldBounds = true;
       pineapple.body.bounce.setTo(1, 1);
@@ -94,9 +88,9 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
 
     this.physics.world.on(
-      "worldbounds",
+      'worldbounds',
       function (body, top, bottom, left, right) {
-        console.log("left", left, "right", right, "top", top, "bottom", bottom);
+        console.log('left', left, 'right', right, 'top', top, 'bottom', bottom);
         if (left) {
           enemy.setVelocityX(100);
         } else {
@@ -114,7 +108,7 @@ export default class HelloWorldScene extends Phaser.Scene {
     enemy2 = this.physics.add.group();
 
     for (let i = 0; i < 13; i++) {
-      var invader = enemy2.create(200 + i * 48, 120, "invader2");
+      var invader = enemy2.create(200 + i * 48, 120, 'invader2');
       //This allows your sprite to collide with the world bounds like they were rigid objects
       invader.body.collideWorldBounds = true;
       invader.body.bounce.setTo(1, 1);
@@ -125,9 +119,9 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
 
     this.physics.world.on(
-      "worldbounds",
+      'worldbounds',
       function (body, top, bottom, left, right) {
-        console.log("left", left, "right", right, "top", top, "bottom", bottom);
+        console.log('left', left, 'right', right, 'top', top, 'bottom', bottom);
         if (left) {
           enemy2.setVelocityX(100);
         } else {
@@ -145,7 +139,7 @@ export default class HelloWorldScene extends Phaser.Scene {
     enemy3 = this.physics.add.group();
 
     for (let i = 0; i < 13; i++) {
-      invader = enemy2.create(200 + i * 48, 190, "invader3");
+      invader = enemy2.create(200 + i * 48, 190, 'invader3');
       //This allows your sprite to collide with the world bounds like they were rigid objects
       invader.body.collideWorldBounds = true;
       invader.body.bounce.setTo(1, 1);
@@ -156,9 +150,9 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
 
     this.physics.world.on(
-      "worldbounds",
+      'worldbounds',
       function (body, top, bottom, left, right) {
-        console.log("left", left, "right", right, "top", top, "bottom", bottom);
+        console.log('left', left, 'right', right, 'top', top, 'bottom', bottom);
         if (left) {
           enemy3.setVelocityX(100);
         } else {
@@ -173,6 +167,31 @@ export default class HelloWorldScene extends Phaser.Scene {
     Phaser.Actions.IncX(enemy.getChildren(), 100);
     Phaser.Actions.IncX(enemy2.getChildren(), 100);
     Phaser.Actions.IncX(enemy3.getChildren(), 100);
+
+    this.physics.add.collider(
+      this.bullets,
+      enemy,
+      function (bulletCollide, enemyCollide) {
+        enemyCollide.destroy();
+        bulletCollide.destroy();
+      }.bind(this)
+    );
+    this.physics.add.collider(
+      this.bullets,
+      enemy2,
+      function (bulletCollide, enemyCollide) {
+        enemyCollide.destroy();
+        bulletCollide.destroy();
+      }.bind(this)
+    );
+    // this.physics.add.collider(
+    //   this.bullets,
+    //   enemy3,
+    //   function (bulletCollide, enemy3Collide) {
+    //     enemy3Collide.destroy();
+    //     bulletCollide.destroy();
+    //   }.bind(this)
+    // );
   }
 
   update() {
